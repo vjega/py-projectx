887c0d995ddfd38d1e34f342c589ab88ec6c1c27
deepak.r <deepak.r@msystechnologies.com>
2016-05-03 15:34:22 +0530

    Sample theme created

--- portal/views.py
+++ portal/views.py
@@ -1,4 +1,10 @@
+from django.shortcuts import render
+from projectx.models import Employee, Menus
 from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def employee(request):
+	return render(request, 'employee.html')
+
