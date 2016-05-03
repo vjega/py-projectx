731104a35d8943b90b47503da8af017744bb8d89
Muthu <mnmuthumca@gmail.com>
2016-05-03 11:30:11 +0530

    employee class created in models.py

--- projectapp/settings.py
+++ projectapp/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         # 'NAME': os.path.join(BASE_DIR, 'my.cnf'),
         'OPTIONS': {
-            'read_default_file': os.path.join(BASE_DIR, 'my.cnf'),
+            'read_default_file': os.path.join(BASE_DIR, 'my.conf'),
         },
     }
 }
